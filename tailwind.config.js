40d8becd39f4714002074e18071fe557f98cdf80
Arjun Deokhile <deokhilea@gmail.com>
2025-07-06 18:22:44 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
     './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
-     colors: {
-        vscode: '#252526',
+      colors: {
+        vscode: '#252526', // custom color
       },
     },
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),[react()],
+ 
   ],
-  base: process.env.VITE_BASE_PATH || "react_vite_deploy",
+  base: process.env.VITE_BASE_PATH || '/react_vite_deploy/',
 })
+
+
+
